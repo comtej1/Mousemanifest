f7e76a95e4b795b03df73674175d7e1b6e013631
comtej1 <114111291+comtej1@users.noreply.github.com>
2022-10-02 16:16:05 -0500

    Update Mousemanifest.R

--- R/Mousemanifest.R
+++ R/Mousemanifest.R
@@ -90,7 +90,7 @@ library(tidyverse)
 library(here)
 
 ##Read in excel sheet with probe info
-manifestFile <- "../../../Mousemanifest4/MouseMethylation-12v1-0_A2.csv"
+manifestFile <- "/tmp/RtmpKT0cxU/filec8c9d4fb53168/Mousemanifest4/MouseMethylation-12v1-0_A2.csv"
 
 maniTmp <- read.manifest.285k(manifestFile)
 
